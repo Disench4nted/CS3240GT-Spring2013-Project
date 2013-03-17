e0b8353d5ff08ac5a6b51a451adbe041b50cf499
Nirav B <bnirav23@gmail.com>
2013-03-17 02:01:39 -0400

    Edited main

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,7 @@ import project.regex.*;
 public class Driver {
 
 	public static void main(String[] args) throws SyntaxError{
-		RegexParser parser = new RegexParser("["+(char)92+"*a-z]");
+		RegexParser parser = new RegexParser("[a-z]+");
 		parser.parse();
 	}
 	
